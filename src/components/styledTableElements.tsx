40e04e315ae32dc8851a251536d21dcabae3e49a
Anton Musatov <anton.musatov@firstlinesoftware.com>
2021-11-01 16:12:06 +0300

    Style changes

--- src/components/styledTableElements.tsx
+++ src/components/styledTableElements.tsx
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 
 export const Table = styled.table`
   min-width: 100%;
+  border-spacing: 0px;
   border-collapse: collapse;
   box-shadow: 0px 0px 11px 4px rgba(0, 0, 0, 0.21);
 `;
@@ -9,21 +10,17 @@ export const Table = styled.table`
 export const THead = styled.thead`
   font-size: 1.35rem;
   background-color: #fff;
-
-  th {
-    border: 1px solid #000;
-  }
 `;
 export const TBody = styled.tbody`
-background-color: #1f1f1f;
-color: #fff;
+  background-color: #1f1f1f;
+  color: #fff;
   th {
-    font-weight: 400;
-    border: 1px solid #aaaaaa;
+    border-color: #fff;
   }
 `;
 export const Tr = styled.tr``;
 
 export const Th = styled.th`
   padding: 40px 20px;
+  border: 1px solid #000;
 `;
